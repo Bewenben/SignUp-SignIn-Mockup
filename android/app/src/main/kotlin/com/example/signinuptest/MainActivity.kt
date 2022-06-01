32a9f0d79f4779acba5d42cdab25ad0b00a47ed5
Bewenben <o.k.salem1@student.aast.edu>
2022-06-01 21:27:32 +0200

    First Commit

--- android/app/src/main/kotlin/com/example/signinuptest/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/signinuptest/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.login_page
+package com.example.signinuptest
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/home/homescreen.dart
+++ lib/home/homescreen.dart
@@ -1,8 +1,7 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class UserInfoScreen extends StatelessWidget {
-  const UserInfoScreen({ Key? key, required User user }) : super(key: key);
+  const UserInfoScreen({ Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
